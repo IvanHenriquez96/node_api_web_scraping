69318518bde696a6a336e8cf1b118e56bacb3829
Ivan Henriquez <ivan.henriquez@omegaccs.com>
2023-06-20 16:05:07 -0400

    puppeteer launch --no-sandbox

--- Controllers/JuegoController.js
+++ Controllers/JuegoController.js
@@ -51,7 +51,7 @@ const scrapper = async (url_game) => {
     //Hace Scraping
     console.log("scrapeando", url_game);
 
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ headless: "new", args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.setViewport({
       width: 1920,
